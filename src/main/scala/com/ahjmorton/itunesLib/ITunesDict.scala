7ae0d3ea87c23fe6406cd6239a0115d26ee64164
Andrew Morton <ahjmorton@gmail.com>
2014-11-22 20:59:46 +0000

    Added first file without test

---
+++ src/main/scala/com/ahjmorton/itunesLib/ITunesDict.scala
@@ -0,0 +1,33 @@
+package com.ahjmorton.itunesLib
+
+import scala.xml.Node
+
+class ITunesDict(xml:Node) {
+
+    private val index = (xml \ "_").grouped(2).map {
+        case Seq(keyNode, elementNode) => (keyNode.text, elementNode)
+    }.toMap
+
+    private def findAndConvert[T >: Null](key:String, conv:(Node => T)) : T = {
+        if(index contains key) 
+            conv(index(key)) 
+        else
+            null
+    }
+
+    def getString = findAndConvert(_:String, (node) => node.text)
+
+    def getInt = findAndConvert(_:String, (node) => node.text.toInt) match { 
+        case a:Int => a;
+        case _ => 0
+    }
+
+    def getBoolean = findAndConvert(_:String, (node) => node.label == "true") match { 
+         case a:Boolean => a 
+         case _ => false
+    }
+
+    def getDict = findAndConvert(_:String, (node) => new ITunesDict(node)) 
+
+    def getArrayOfDicts = findAndConvert(_:String, (node) => (node \ "_").map(new ITunesDict(_)))
+}
